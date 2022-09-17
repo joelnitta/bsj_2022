6035c4a33012d86b16d560b745e3a03463c012a1
joelnitta <joelnitta@gmail.com>
2022-09-17 11:52:16 +0900

    Update slides
    add more qr codes

--- R/qrcode.R
+++ R/qrcode.R
@@ -2,10 +2,22 @@ library(qrcode)
 
 # Generate QR codes
 
+# - slides
 png("images/slides_qr.png")
 qrcode_gen("https://joelnitta.github.io/bsj_2022/")
 dev.off()
 
+# - paper
 png("images/ftol_paper_qr.png")
 qrcode_gen("https://doi.org/10.3389/fpls.2022.909768")
+dev.off()
+
+# - ftol site
+png("images/ftol_site_qr.png")
+qrcode_gen("https://fernphy.github.io/")
+dev.off()
+
+# - ftolr
+png("images/ftolr_qr.png")
+qrcode_gen("https://github.com/fernphy/ftolr")
 dev.off()
